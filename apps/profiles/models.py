e02020b4fdde426bc3097adcef4b3bc71e20ad11
E <eric@ubuntupc.(none)>
2010-08-07 17:05:44 -0500

    add some contact fields and a timezone for each user

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from tagging.fields import TagField
 from tagging.models import Tag
+from timezones.fields import TimeZoneField
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
+    skype = models.CharField("Skype ID", max_length=20, null=True, blank=True)
+    aim = models.CharField("AIM", max_length=20, null=True, blank=True)
+    gchat = models.CharField("Google Chat", max_length=20, null=True, blank=True)
+    phone = models.CharField("Phone number", max_length=20, null=True, blank=True)
+    timezone = TimeZoneField()
+
     skills = TagField()
     def set_tags(self, tags):
         Tag.objects.update_tags(self, tags)
