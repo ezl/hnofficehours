1e23eb22328e208cb44c2ca371471c1af659d79f
E <eric@ubuntupc.(none)>
2010-08-07 17:40:34 -0500

    merge

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -4,16 +4,23 @@ from django.template import RequestContext
 from django.views.generic.simple import direct_to_template
 
 from profiles.models import Profile
-from profiles.forms import SkillsForm, ProfileForm
+from profiles.forms import ProfileForm, ProfileSkillsForm
+from django.contrib.auth.decorators import login_required
 
-
-def skills_test(request):
-    skills_form = SkillsForm()
-    return direct_to_template(request, 'profiles/skills_test.html',
-                              {'skills_form':skills_form})
 
 def userprofile(request, username=None, template_name='profiles/profile.html'):
     form = ProfileForm()
     return render_to_response(template_name, locals(),
                               context_instance=RequestContext(request))
 
+#@login_required
+def profile(request):
+    if request.method == "POST":
+        #import ipdb; ipdb.set_trace()
+        tag_list = request.POST.get('skills_text').strip(',').split(' ')
+        for tag in tag_list:
+            skill, created = Skill.objects.get_or_create(name=name)
+    else:
+        profile_form = ProfileSkillsForm()
+    return direct_to_template(request, 'profiles/skills_test.html', 
+                                        {'profile_form':profile_form})
