72c5f743c6b4c148e536400b80f7dcec997dafd2
E <eric@ubuntupc.(none)>
2010-08-07 17:39:03 -0500

    merge

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -2,10 +2,13 @@ from models import Profile
 from django import forms
 from tagging.forms import TagField
 from tagging_autocomplete.widgets import TagAutocomplete
+from profiles.models import *
+from ajax_select.fields import AutoCompleteSelectMultipleField, AutoCompleteSelectField
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
 
-class SkillsForm(forms.Form):
-    skills = TagField(widget=TagAutocomplete())
+class ProfileSkillsForm(forms.Form):
+    # declare a field and specify the named channel that it uses
+    skills = AutoCompleteSelectMultipleField('profile_skills', required=False)
