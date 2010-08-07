08c478f8d08b3e218f6e367ef302ab2096df9c0f
E <eric@ubuntupc.(none)>
2010-08-07 15:20:47 -0500

    display error message on re-registration attempts

--- apps/registration/forms.py
+++ apps/registration/forms.py
@@ -2,6 +2,7 @@ import urllib2
 from django import forms
 from django.conf import settings
 from retrieve_hn_user_data import retrieve_hn_user_data
+from django.contrib.auth.models import User
 
 
 class RegistrationForm(forms.Form):
@@ -16,6 +17,7 @@ class RegistrationForm(forms.Form):
             raise
         if not hnoh_profile in user_data["about"]:
             raise forms.ValidationError('The url does not appear to be in your hn profile. Please paste %s into your hn profile. Note the trailing slash.' % hnoh_profile)
+        if User.objects.filter(username=hn_username).count() > 0:
+            errormsg = 'User "%s" already has an account' % hn_username
+            raise forms.ValidationError(errormsg)
         return self.cleaned_data
-
-
