8818005e8fcf6355490ed9e8079f82e847ab4e26
Chris Allen <cma@bitemyapp.com>
2010-08-07 16:25:06 -0400

    got blank skills page up

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,9 +1,8 @@
 from django.views.generic.simple import direct_to_template
-from profile.models import Profile
-from profile.forms import SkillsForm
+from profiles.models import Profile
+from profiles.forms import SkillsForm
 
 def skills_test(request):
     skills_form = SkillsForm()
-    direct_to_template(request, 
-                       'profiles/skills_test.html', 
-                       {'skills_form':skills_form})
+    return direct_to_template(request, 'profiles/skills_test.html', 
+                       				  {'skills_form':skills_form})
