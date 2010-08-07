fbcd98530829fd38556d061c12bf84a944ddd9e5
Chris Allen <cma@bitemyapp.com>
2010-08-07 15:17:41 -0400

    urls, setting up test view

--- apps/profile/forms.py
+++ apps/profile/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from tagging.forms import TagField
 from tagging_autocomplete.widgets import TagAutocomplete
 
-class TestTags(forms.Form):
+class SkillsForm(forms.Form):
     skills = TagField(widget=TagAutocomplete())
