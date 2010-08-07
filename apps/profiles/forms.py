1e61e0f37ea4e7b1b5c7b29b93188f72dd793e15
E <eric@ubuntupc.(none)>
2010-08-07 17:06:55 -0500

    add ModelForm for UserProfile

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -1,6 +1,11 @@
+from models import Profile
 from django import forms
 from tagging.forms import TagField
 from tagging_autocomplete.widgets import TagAutocomplete
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
 
 class SkillsForm(forms.Form):
     skills = TagField(widget=TagAutocomplete())
