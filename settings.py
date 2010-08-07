a72762116e9296cd3443e4a75e87947be16337d5
Chad Glendenin <chad@glendenin.com>
2010-08-07 13:25:47 -0500

    installed django-schedule app and request templ. context proc.

--- settings.py
+++ settings.py
@@ -69,6 +69,14 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -95,8 +103,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
-    
+
     'django_extensions',
+    'schedule',
 
     'registration',
 )
