8f511ae8c172870a3647d17a94eb4c5d178f0ed8
E <eric@ubuntupc.(none)>
2010-08-07 17:54:04 -0500

    remove dead view from urls

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('profiles.views',
     url(r'^(?P<username>\w+)/$', 'userprofile', name='userprofile'),
-    url(r'^', 'skills_test', name='skills-test'),
     url(r'^', 'profile', name='profile'),
 )
