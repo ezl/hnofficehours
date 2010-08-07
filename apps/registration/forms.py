b564b4f1b53638990d4d11c3577acff236013d05
E <eric@ubuntupc.(none)>
2010-08-07 17:04:25 -0500

    remove gibberish line

--- apps/registration/forms.py
+++ apps/registration/forms.py
@@ -16,7 +16,6 @@ class RegistrationForm(forms.Form):
         except:
             errormsg = "%s does not appear to be a registered HN username" % hn_username
             raise forms.ValidationError(errormsg)
-            print "adsfhaldfadsf"
         if not hnoh_profile in user_data["about"]:
             raise forms.ValidationError('The url does not appear to be in your hn profile. Please paste %s into your hn profile. Note the trailing slash.' % hnoh_profile)
         if User.objects.filter(username=hn_username).count() > 0:
