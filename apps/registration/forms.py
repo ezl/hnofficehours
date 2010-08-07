1afc1943223680ddbdaeb7fb9ad9c409c5188dfa
E <eric@ubuntupc.(none)>
2010-08-07 15:01:53 -0500

    use retrieve_hn_user_data.py

--- apps/registration/forms.py
+++ apps/registration/forms.py
@@ -1,6 +1,7 @@
 import urllib2
 from django import forms
 from django.conf import settings
+from retrieve_hn_user_data import retrieve_hn_user_data
 
 
 class RegistrationForm(forms.Form):
@@ -8,10 +9,12 @@ class RegistrationForm(forms.Form):
 
     def clean(self):
         hn_username = self.cleaned_data['hn_username']
-        hn_profile_url = "http://news.ycombinator.com/user?id=%s" % hn_username
         hnoh_profile =  settings.USER_PROFILE_URL % hn_username
-        page = urllib2.urlopen(hn_profile_url).read()
-        if not hnoh_profile in page:
+        try:
+            user_data = retrieve_hn_user_data(hn_username)
+        except:
+            raise
+        if not hnoh_profile in user_data["about"]:
             raise forms.ValidationError('The url does not appear to be in your hn profile. Please paste %s into your hn profile. Note the trailing slash.' % hnoh_profile)
         return self.cleaned_data
 
--- apps/registration/views.py
+++ apps/registration/views.py
@@ -13,6 +13,9 @@ from django.template import RequestContext
 
 from registration.forms import RegistrationForm
 
+from retrieve_hn_user_data import retrieve_hn_user_data
+
+
 def register(request, template_name='registration/register.html'):
     if request.method == 'GET':
         profile_url = settings.USER_PROFILE_URL % 'username'
@@ -20,13 +23,19 @@ def register(request, template_name='registration/register.html'):
     else: # POST
         form = RegistrationForm(data=request.POST)
         if form.is_valid():
+            hn_username = form.cleaned_data['hn_username']
+            # user_data is a dict containing the users HN account creation
+            # date, karma at query time, and HN profile
+            user_data = retrieve_hn_user_data(hn_username)
             # form validation checks that hn_username matches what's provided
             # and that the "key" (a url) is in the profile
             temp_password = ''.join(random.sample(string.letters, 16))
-            new_user = User.objects.create_user(
-                                    username=form.cleaned_data['hn_username'],
-                                    email='', password=temp_password)
-            user = authenticate(username=new_user.username, password=temp_password)
+            new_user = User.objects.create_user(username=hn_username,
+                                                email='',
+                                                password=temp_password)
+            user = authenticate(username=new_user.username,
+                                password=temp_password)
+            
             if user is not None:
                 login(request, user)
                 messages.success(request, 'That worked. Please provide a password.')
