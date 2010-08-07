51c662a5609e5c87184ee627bfd67fd6b7da498f
E <eric@ubuntupc.(none)>
2010-08-07 17:53:01 -0500

    fix broken merge

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import User
 from tagging.fields import TagField
 from tagging.models import Tag
 from tagging_autocomplete.models import TagAutocompleteField
-<<<<<<< HEAD
 from timezones.fields import TimeZoneField
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
@@ -16,11 +14,6 @@ class Profile(models.Model):
     phone = models.CharField("Phone number", max_length=20, null=True, blank=True)
     timezone = TimeZoneField()
 
-=======
-
-class Profile(models.Model):
-    user = models.OneToOneField(User)
->>>>>>> 3f3a784d2ebea8c8e7104cfb80e1c547b051eeac
     skills = models.ManyToManyField('Skill', blank=True)
     def __unicode__(self):
         return unicode(self.user)
