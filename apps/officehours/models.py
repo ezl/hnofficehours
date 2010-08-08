9a9549b4d6e5cb18ba2722ebb6f81e306ec58a08
Rodrigo Guzman <rodguze@gmail.com>
2010-08-08 14:21:05 -0500

    Got rid of some unused code.

--- apps/officehours/models.py
+++ apps/officehours/models.py
@@ -1,23 +1,8 @@
-# from django.contrib.auth.models import User
-# from django.db import models
-# from django.db.models import signals
 from django.db import models
 from django.contrib.sites.models import Site
 import schedule.models
 from schedule.models import Calendar
 
-
-# def create_user_calendar(sender, instance=None, **kwargs):
-#     """
-#     Whenever a User object is created/saved, make sure there's Calendar
-#     object associated with that User. The django-schedule looks up calendars
-#     by slug, so just make the slug the User's username.
-#     """
-#     if not instance:
-#         return
-#     cal = Calendar.objects.get_or_create_calendar_for_object(instance,
-#                                                          name=instance.username)
-# signals.post_save.connect(create_user_calendar, sender=User)
 
 def create_global_calendar(sender=None, app=None, **kwargs):
     if not sender:
