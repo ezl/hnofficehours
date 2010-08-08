710cbff07653dba76056207dc8adc9aba828320d
E <eric@ubuntupc.(none)>
2010-08-07 19:54:25 -0500

    i love nutella

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('profiles.views',
-    url(r'^(?P<username>\w+)/$', 'userprofile', name='userprofile'),
     url(r'^$', 'profile', name='profile'),
     url(r'^list/$', 'list_profiles', name='list_profiles'),
     url(r'^(?P<username>\w+)/$', 'view_profile', name='view_profile'),
