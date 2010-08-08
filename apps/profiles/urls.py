ce69e5fbdd9d744530615c11d7d3e0e5df4cc852
E <eric@ubuntupc.(none)>
2010-08-07 19:42:07 -0500

    create view that lists all users

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('profiles.views',
     url(r'^$', 'profile', name='profile'),
+    url(r'^list/$', 'list_profiles', name='list_profiles'),
     url(r'^(?P<username>\w+)/$', 'view_profile', name='view_profile'),
 )
