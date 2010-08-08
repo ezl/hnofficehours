de14bded80c2258d1983cf8d3978f4fa91a40772
crccheck <c@crccheck.com>
2010-08-08 11:43:49 -0500

    add structure to template directory for easier modification later

--- views.py
+++ views.py
@@ -1,16 +1,21 @@
-import datetime
+from datetime import datetime, timedelta
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
-from officehours.forms import EventForm
-from schedule.utils import check_event_permissions
-from schedule.models import Calendar, Rule
-from schedule.utils import coerce_date_dict
-from schedule.views import get_next_url
+from schedule.models import Event
 
 
 def site_index(request, template_name='index.html'):
+    def _format_office_hour(office_hour):
+        # get the office hour holder, skills, contact methods
+        return office_hour
+    events = Event.objects.all()
+    start = datetime.now()
+    end = start + timedelta(days=7)
+    office_hours = reduce(lambda x,y: x+y, [e.get_occurrences(start, end)
+                                            for e in events]) if events else []
+    office_hours = map(_format_office_hour, office_hours)
     return render_to_response(template_name, locals(),
                               context_instance=RequestContext(request))
