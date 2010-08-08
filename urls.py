077e8634433b23a5ceb7c0b9ddd761f8276598c8
crccheck <c@crccheck.com>
2010-08-08 11:05:51 -0500

    rename original proposal page for clarity

--- urls.py
+++ urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^hnofficehours/', include('hnofficehours.foo.urls')),
-    (r'^proposal/$', 'django.views.generic.simple.direct_to_template', {'template': 'homepage.html'}),
     (r'^tagging_autocomplete/', include('tagging_autocomplete.urls')),
     (r'^ajax_select/', include('ajax_select.urls')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
@@ -25,6 +24,7 @@ urlpatterns = patterns('',
     url(r'^profile/', include('profiles.urls')),
     url(r'^$', 'django.views.generic.simple.direct_to_template',
         {'template':'index.html'}, name='index'),
+    (r'^proposal/$', 'django.views.generic.simple.direct_to_template', {'template': 'originalproposal.html'}),
 )
 
 if settings.DEBUG:
