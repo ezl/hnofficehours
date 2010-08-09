e3900ebdaf80b98b730f590d970ab1b27f4d5e45
Chad Glendenin <chad@glendenin.com>
2010-08-08 23:35:31 -0500

    basic model to hold time availability flags last updated

--- apps/availability/models.py
+++ apps/availability/models.py
@@ -1,3 +1,22 @@
+import sys
 from django.db import models
 
-# Create your models here.
+
+class LastUpdate(models.Model):
+    """
+    Just need a convenient place to store the last time the update_availability
+    task ran so that we can define a Period object for the appropriate time
+    delta.
+    """
+    last_update = models.DateTimeField(null=True, auto_now=True)
+
+    def __unicode__(self):
+        return str(self.last_update)
+
+
+def set_first_update(sender=None, app=None, **kwargs):
+    if not sender:
+        return
+    LastUpdate.objects.create()
+this_module = sys.modules[globals()['__name__']]
+models.signals.post_syncdb.connect(set_first_update, sender=this_module)
