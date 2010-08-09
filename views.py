a069cd5b439078905f462f4656dc83885178930a
E <eric@ubuntupc.(none)>
2010-08-08 20:16:24 -0500

    decide who shows up on the front page
    current rule for who gets displayed is:
    (all users who have been marked as available) UNION
    (all users who have current office hours)
    
    logic:
    there will be a function that automatically marks users
    as available/unavailable when their site-set office hours
    begin or end.  users may also just mark themselves as available
    without having office hours (as per conversation on HN thread).
    users may also have current office hours but be occupied (if somebody
    engages them).
    
    all these users should show up in the list.

--- views.py
+++ views.py
@@ -6,9 +6,16 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
 from schedule.models import Event
+from schedule.periods import Period
 
 
 def site_index(request, template_name='index.html'):
-    available_users = User.objects.filter(profile__is_available=True)
+    users_available_now = User.objects.filter(profile__is_available=True)
+    events = Event.objects.all()
+    now = Period(events=events, start=datetime.now(),
+                                end=datetime.now() + timedelta(minutes=1))
+    occurences = now.get_occurrences()
+    users_holding_office_hours_now = map(lambda x: x.event.creator, occurences)
+    users = set(list(users_available_now) + users_holding_office_hours_now)
     return render_to_response(template_name, locals(),
                               context_instance=RequestContext(request))
